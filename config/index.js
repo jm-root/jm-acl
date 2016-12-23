b081429fc776880dd687f091c92c26a1d453d99c
fjy <531480056@qq.com>
2016-12-23 16:46:08 +0800

    完成增删改查

--- config/index.js
+++ config/index.js
@@ -4,7 +4,6 @@ var config = {
         debug: true,
         port: 20110,
         db: 'mongodb://localhost:27017/acl',
-        sdk: 'http://192.168.0.32:20200',
         ms: [
             {type: 'ws'},
             {type: 'http'}
@@ -15,7 +14,6 @@ var config = {
         debug: false,
         port: 20110,
         db: 'mongodb://test:123@mongo:27017/acl',
-        sdk: 'http://sdk:20200',
         ms: [
             {type: 'ws'},
             {type: 'http'}
@@ -31,7 +29,6 @@ config.env = env;
     env = process.env;
     config.port = env.port || config.port;
     config.db = env.db || config.db;
-    config.sdk = env.sdk || config.sdk;
 }
 
 
