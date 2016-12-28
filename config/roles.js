70662234dec4d1fb2a52565c1a6a9ac93041b495
jeff yu <jeff@jamma.cn>
2016-12-29 01:03:38 +0800

    优化role初始化

--- config/roles.js
+++ config/roles.js
@@ -1,5 +1,10 @@
 module.exports = [
     {
+        code: 'superadmin',
+        title: '超级管理员',
+        description: '超级管理员'
+    },
+    {
         code: 'guest',
         title: '访客',
         description: '访客',
@@ -7,45 +12,42 @@ module.exports = [
             {resource: '/config', permissions: ['get']},
             {resource: '/acl', permissions: ['get']},
             {resource: '/sso', permissions: ['get']}
-        ],
-        children: [
-            {
-                code: 'user',
-                title: '用户',
-                description: '已登陆用户',
-                children: [
-                    {
-                        code: 'admin',
-                        title: '系统管理员',
-                        description: '系统管理员'
-                    },
-                    {
-                        code: 'cp',
-                        title: '货币发行商',
-                        description: '货币发行商, 可以发行货币'
-                    },
-                    {
-                        code: 'merchant',
-                        title: '商户',
-                        description: '商户'
-                    },
-                    {
-                        code: 'agent',
-                        title: '一级代理商',
-                        description: '一级代理商'
-                    },
-                    {
-                        code: 'agent2',
-                        title: '二级代理商',
-                        description: '二级代理商'
-                    }
-                ]
-            }
         ]
     },
     {
-        code: 'superadmin',
-        title: '超级管理员',
-        description: '超级管理员'
+        code: 'user',
+        title: '用户',
+        description: '已登陆用户',
+        parents: ['guest']
+    },
+    {
+        code: 'admin',
+        title: '系统管理员',
+        description: '系统管理员',
+        parents: ['user']
+    },
+    {
+        code: 'cp',
+        title: '货币发行商',
+        description: '货币发行商, 可以发行货币',
+        parents: ['user']
+    },
+    {
+        code: 'merchant',
+        title: '商户',
+        description: '商户',
+        parents: ['user']
+    },
+    {
+        code: 'agent',
+        title: '一级代理商',
+        description: '一级代理商',
+        parents: ['user']
+    },
+    {
+        code: 'agent2',
+        title: '二级代理商',
+        description: '二级代理商',
+        parents: ['user']
     }
 ];
