76f3a3d167014bc26de3cbdd7cfdecc19987fa3c
jeff yu <jeff@jamma.cn>
2017-01-13 10:32:38 +0800

    增加reload model.create拦截 优化

--- config/orgs.js
+++ config/orgs.js
@@ -8,23 +8,7 @@ module.exports = [
                 code: 'acl',
                 title: '权限',
                 description: '权限'
-            },
-            {
-                code: 'config',
-                title: '配置',
-                description: '配置'
-            },
-            {
-                code: 'sso',
-                title: '用户',
-                description: '用户'
             }
         ]
-    },
-    {
-        code: 'bank',
-        title: '银行',
-        description: '银行',
-        children: []
     }
 ];
--- config/roles.js
+++ config/roles.js
@@ -9,9 +9,7 @@ module.exports = [
         title: '访客',
         description: '访客',
         allows: [
-            {resource: '/config', permissions: ['get']},
-            {resource: '/acl', permissions: ['get']},
-            {resource: '/sso', permissions: ['get']}
+            {resource: '/acl', permissions: ['get']}
         ]
     },
     {
@@ -24,30 +22,6 @@ module.exports = [
         code: 'admin',
         title: '系统管理员',
         description: '系统管理员',
-        parents: ['user']
-    },
-    {
-        code: 'cp',
-        title: '货币发行商',
-        description: '货币发行商, 可以发行货币',
-        parents: ['user']
-    },
-    {
-        code: 'merchant',
-        title: '商户',
-        description: '商户',
-        parents: ['user']
-    },
-    {
-        code: 'agent',
-        title: '一级代理商',
-        description: '一级代理商',
-        parents: ['user']
-    },
-    {
-        code: 'agent2',
-        title: '二级代理商',
-        description: '二级代理商',
         parents: ['user']
     }
 ];
