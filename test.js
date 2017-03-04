f8394031f099617e3e8e30e25e299276d81c232f
jacsion <179407668@qq.com>
2017-03-04 16:15:39 +0800

    让userResources函数支持指定资源

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 'use strict';
+var _ = require('lodash');
 var service = require('./lib')({
-    db:require('jm-dao').DB.connect('mongodb://localhost:27017/acl'),
+    db:require('jm-dao').DB.connect('mongodb://localhost:27017/acl1'),
     mq:require('jm-mq')({url: 'redis://localhost:6379'})
 });
 setTimeout(function(){
@@ -48,11 +49,17 @@ setTimeout(function(){
     // service.addUserRoles('58885d5dd0ee2812e4035ee8','user').then(function(){
     //     console.log(arguments);
     // });
-    service.isAllowed('58885d5dd0ee2812e4035ee8','/acl/resources','post',function(){
-        console.log(arguments);
-    });
-    service.isAllowed('58885d5dd0ee2812e4035ee8','/acl/resources','post').then(function(){
-        console.log(arguments);
-    });
+    // service.isAllowed('58885d5dd0ee2812e4035ee8','/acl/resources','post',function(){
+    //     console.log(arguments);
+    // });
+    // service.isAllowed('58885d5dd0ee2812e4035ee8','/acl/resources','post').then(function(){
+    //     console.log(arguments);
+    // });
+    // service.userResources('57563e1430a23d5239ea2068',function(err,ret){
+    //     console.log(ret);
+    // });
+    // service.userResources('57563e1430a23d5239ea2068', '/acl/roles',function(err,ret){
+    //     console.log(ret);
+    // });
 
 },1000);
