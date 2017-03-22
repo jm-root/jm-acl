ae19e75d021448f4d96188946ba99b88812d8888
jammacn <jeff@jamma.cn>
2017-03-22 09:40:52 +0800

    重写isAllowed，支持资源树

--- config/resources.js
+++ config/resources.js
@@ -1,6 +1,9 @@
 module.exports = [
     {
-        code: 'global', title: '全局权限', permissions: ['put', 'delete', 'post', 'get']
+        code: 'global', title: '全局权限', permissions: ['put', 'delete', 'post', 'get'],
+        children: [
+            { code: 'global/search', title: '搜索', permissions: ['get'] }
+        ]
     },
     {
         code: '/acl', title: '权限', permissions: ['get'],
@@ -14,18 +17,26 @@ module.exports = [
             {
                 code: '/acl/roles', title: '角色', permissions: ['put', 'delete', 'post', 'get'],
                 children: [
-                    {code: '/acl/roles/:id', title: '指定角色', permissions: ['put', 'delete', 'post', 'get']},
-                    {code: '/acl/roles/:id/users', title: '指定角色的用户', permissions: ['get']},
-                    {code: '/acl/roles/:id/resources', title: '指定角色的资源', permissions: ['get', 'delete']},
-                    {code: '/acl/roles/:id/permissions', title: '指定角色的权限', permissions: ['post', 'delete']}
+                    {
+                        code: '/acl/roles/:id', title: '指定角色', permissions: ['put', 'delete', 'post', 'get'],
+                        children: [
+                            {code: '/acl/roles/:id/users', title: '指定角色的用户', permissions: ['get']},
+                            {code: '/acl/roles/:id/resources', title: '指定角色的资源', permissions: ['get', 'delete']},
+                            {code: '/acl/roles/:id/permissions', title: '指定角色的权限', permissions: ['post', 'delete']}
+                        ]
+                    },
                 ]
             },
             {
                 code: '/acl/users', title: '系统用户', permissions: ['put', 'delete', 'post', 'get'],
                 children: [
-                    {code: '/acl/users/:id', title: '指定系统用户', permissions: ['put', 'delete', 'post', 'get']},
-                    {code: '/acl/users/:id/roles', title: '指定用户的角色', permissions: ['get']},
-                    {code: '/acl/users/:id/permissions', title: '指定用户的权限', permissions: ['get']}
+                    {
+                        code: '/acl/users/:id', title: '指定系统用户', permissions: ['put', 'delete', 'post', 'get'],
+                        children: [
+                            {code: '/acl/users/:id/roles', title: '指定用户的角色', permissions: ['get']},
+                            {code: '/acl/users/:id/permissions', title: '指定用户的权限', permissions: ['get']}
+                        ]
+                    }
                 ]
             }
         ]
--- test/acl.js
+++ test/acl.js
@@ -12,18 +12,33 @@ if((typeof exports !== 'undefined' && typeof module !== 'undefined')){
         if (err) {
             logger.error(err.stack);
         }
-        if(doc){
-            logger.debug('%s', utils.formatJSON(doc));
-        }
+        logger.debug('%s', utils.formatJSON(doc));
     };
 
     var opts = {
-        db: require('jm-dao').DB.connect()
+        db: require('jm-dao').DB.connect('mongodb://localhost/acl')
     };
+    var service = require('../lib')(opts);
 
-    opts.db.on('open', function(){
-        var service = require('../lib')(opts);
-        service.acl.allow('guest', '/acl', 'get', log);
-        service.acl.addUserRoles('guest', 'guest', log);
+    // opts.db.on('open', function(){
+    //     service.acl.allow('guest', '/acl', 'get');
+    //     service.acl.allow('guest', '/acl/resources', 'put');
+    //     service.acl.allow('guest', '/acl/resources/:id', 'delete');
+    //     service.acl.addUserRoles('guest', 'guest');
+    // });
+
+    service.on('loaded', (name) => {
+        if (name !== 'resource') return;
+        log(null, 'isAllowed /acl get,post');
+        //service.isAllowed(null, '/acl', ['get', 'post'], log);
+        //service.isAllowed(null, '/acl/resources', 'put', log);
+        service.isAllowed(null, '/acl/resources1', 'post', log);
+        // service.isAllowed(null, '/acl/resources/abc', 'delete', log);
     });
+
 })();
+
+process.on('uncaughtException', function (err) {
+    console.error('Caught exception: ' + err.stack);
+});
+
